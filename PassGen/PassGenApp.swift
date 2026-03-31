38a96805115e12bed223c9fca0ee5dbcb7ba1666
Denys Popov <denyspopov.ua@gmail.com>
2026-03-31 13:03:55 +0300

    Initial Commit

---
+++ PassGen/PassGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassGenApp.swift
+//  PassGen
+//
+//  Created by Denys Popov on 31.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct PassGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
